b1f827381b9e9e17039086b310fa2369ce00e213
Kyle Yu <kyle@KyledeMacBook-Air.local>
2026-04-23 23:52:51 +0800

    Switch site URL to custom domain

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import { uiText } from './i18n'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://blog-virid-five-16.vercel.app',
+  site: 'https://kkkkkkkyle.com',
   // The name of your site, used in the title and for SEO.
   title: 'Kyle\'s Blog',
   // The description of your site, used for SEO and RSS feed.
